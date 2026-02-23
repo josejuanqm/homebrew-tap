fb04bde8eca4e5a52254381f784e518eb72f5ce2
Jose Quintero <jose.juan.qm@gmail.com>
2026-02-23 11:24:06 -0600

    first commit

---
+++ Formula/melody.rb
@@ -0,0 +1,12 @@
+class Melody < Formula
+  desc "Declarative mobile app framework"
+  homepage "https://github.com/josejuanqm/melody"
+  url "https://github.com/josejuanqm/melody/releases/download/v0.1.0/melody-macos-universal.tar.gz"
+  sha256 "c0ef1a8892e52d9b058334e2898d934e4b48eb7b2afcc548db9575c08afb2032"
+  license "MIT"
+  version "0.1.0"
+
+  def install
+    bin.install "melody"
+  end
+end
